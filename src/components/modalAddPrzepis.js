8866e0488e6e5c40cc0f68a2c7acab1559de7f13
Albina Lapchevska <albina.lapchevska@gmail.com>
2023-12-16 18:25:57 +0100

    Add more fields

--- src/components/modalAddPrzepis.js
+++ src/components/modalAddPrzepis.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import InfoIcon from "@mui/icons-material/Info";
-
+import TextField from '@mui/material/TextField';
 import { useAppDispatch, useAppSelector } from "../app/hooks.js";
 import { closeDialogs } from "../redux/slices/authSlice.js";
+import Autocomplete from '@mui/material/Autocomplete';
+import {
+    useGetCategoriesQuery,
+} from "../redux/api/index.js";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const ModalAddPrzepis = () => {
     const { isModalAddPrzepisOpen } = useAppSelector(
@@ -22,6 +26,39 @@ const ModalAddPrzepis = () => {
         dispatch(closeDialogs(false));
     };
 
+    const poziomyTrudnosci = [
+        { label: 'Łatwe' },
+        { label: 'Średnie' },
+        { label: 'Ciężkie' },
+    ];
+    const liczbaPorcji = [
+        { label: '1' },
+        { label: '2' },
+        { label: '3' },
+        { label: '4' },
+        { label: '5' },
+        { label: '6' },
+        { label: '7' },
+        { label: '8' },
+        { label: '9' },
+        { label: '10' },
+    ];
+    const { data: categories } = useGetCategoriesQuery();
+
+    const kategorieLista = [
+        { label: '' },
+        ...(categories || []).map(category => ({ label: category.name }))
+    ];
+
+    const [numericValue, setNumericValue] = useState('');
+
+    const handleNumericChange = (event) => {
+        const enteredValue = event.target.value;
+        const onlyNums = enteredValue.replace(/\D/g, ''); // Keep only numeric characters
+
+        setNumericValue(onlyNums);
+    };
+
     return (
         <Dialog fullScreen open={isModalAddPrzepisOpen} onClose={handleCloseDialog}>
             <DialogTitle
@@ -29,11 +66,9 @@ const ModalAddPrzepis = () => {
                     textAlign: "center",
                     backgroundColor: "rgb(195, 172, 214)",
                 }}>
-
                 <Typography variant="h5" style={{ fontWeight: "bold" }}>
-                    Тест
+                    Dodawanie własnego przepisu
                 </Typography>
-
                 <IconButton
                     edge="end"
                     style={{ position: "absolute", right: 20, top: 10 }}
@@ -43,10 +78,55 @@ const ModalAddPrzepis = () => {
                     <CloseIcon />
                 </IconButton>
             </DialogTitle>
-            <DialogContent style={{ textAlign: "left" }}>
+            <DialogContent style={{ textAlign: "left", paddingTop: '15px' }}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ display: 'flex', gap: '16px' }}>
+                        <TextField
+                            id="outlined-basic"
+                            label="Wpisz nazwę"
+                            variant="outlined"
+                            style={{ width: 500 }}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo-poziomy"
+                            options={poziomyTrudnosci}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Wybierz poziom trudności" />}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo-kategorie"
+                            options={kategorieLista}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Wybierz kategorię" />}
+                        />
+                    </div>
 
+                    <div style={{ display: 'flex', gap: '16px', paddingTop: '15px' }}>
+                        <TextField
+                            id="outlined-basic-numbers"
+                            label="Wpisz czas w minutach"
+                            variant="outlined"
+                            value={numericValue}
+                            onChange={handleNumericChange}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo-poziomy"
+                            options={liczbaPorcji}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Wybierz liczbę porcji" />}
+                        />
+                        <FormGroup>
+                            <FormControlLabel control={<Checkbox defaultChecked />} label="Ten przepis jest wegański" />
+                        </FormGroup>
+                    </div>
+                    <Typography variant="h6"  style={{  paddingTop: '5px' }}>
+                       Dodaj składniki
+                    </Typography>
+                </div>
             </DialogContent>
-
         </Dialog>
     );
 };
