b5b6f7395863e112261764c1dbaeb650b10974f5
Albina Lapchevska <albina.lapchevska@gmail.com>
2023-12-17 19:44:56 +0100

    remove unused variables

--- src/components/krokiComponent.js
+++ src/components/krokiComponent.js
@@ -1,20 +1,11 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import {
-    useGetAllProductsQuery,
-} from "../redux/api/index.js";
+
 import React, { useState } from "react";
 
 
 const KrokiComponent = () => {
-    const { data: products } = useGetAllProductsQuery();
-    const produktyLista = [
-        { label: '' },
-        ...(products || []).map(category => ({ label: `${category.name} - ${category.unit}` }))
-    ];
-
     return (
         <div style={{ display: 'flex', gap: '20px', paddingBottom: '10px' }}>
             <TextField
